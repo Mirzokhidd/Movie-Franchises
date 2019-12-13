e18d0145dc589193487f0026511769441830f4e5
Mirzokhidd <50044566+Mirzokhidd@users.noreply.github.com>
2019-12-13 21:25:16 +0400

    Add files via upload

---
+++ java.js
@@ -0,0 +1,80 @@
+function openStarWars() {
+    document.getElementById("starwars").style.height = "100%";
+}
+
+function closeStarWars() {
+    document.getElementById("starwars").style.height = "0%";
+}
+
+function openIJ() {
+    document.getElementById("indianajohns").style.height = "100%";
+}
+
+function closeIJ() {
+    document.getElementById("indianajohns").style.height = "0%";
+}
+
+function openST() {
+    document.getElementById("startrek").style.height = "100%";
+}
+
+function closeST() {
+    document.getElementById("startrek").style.height = "0%";
+}
+function openHP() {
+    document.getElementById("harrypotter").style.height = "100%";
+}
+
+function closeHP() {
+    document.getElementById("harrypotter").style.height = "0%";
+}
+
+function openJB() {
+    document.getElementById("jamesbond").style.height = "100%";
+}
+
+function closeJB() {
+    document.getElementById("jamesbond").style.height = "0%";
+}
+function openFF() {
+    document.getElementById("FastFuriuse").style.height = "100%";
+}
+
+function closeFF() {
+    document.getElementById("FastFuriuse").style.height = "0%";
+}
+function openTR() {
+    document.getElementById("transformers").style.height = "100%";
+}
+
+function closeTR() {
+    document.getElementById("transformers").style.height = "0%";
+}
+function openXM() {
+    document.getElementById("xmen").style.height = "100%";
+}
+
+function closeXM() {
+    document.getElementById("xmen").style.height = "0%";
+}
+function openJP() {
+    document.getElementById("jurassicpark").style.height = "100%";
+}
+
+function closeJP() {
+    document.getElementById("jurassicpark").style.height = "0%";
+}
+
+
+function openMI() {
+    document.getElementById("missionimpossible").style.height = "100%";
+}
+
+function closeMI() {
+    document.getElementById("missionimpossible").style.height = "0%";
+}
+
+
+
+
+
